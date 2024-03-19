810c89c34b2d503610d2033e4a164ddd7e9866ee
Anastasia <nassstya442@gmail.com>
2024-03-19 22:03:28 +0300

    hh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        TreeFormula tree = new TreeFormula(str);
+        System.out.println(tree.calculate());
     }
 }
---
+++ src/TreeFormula.java
@@ -0,0 +1,65 @@
+
+
+public class TreeFormula {
+    private Node head;
+    private static int i;
+
+    public TreeFormula(String str) throws Exception{
+        this.head = buildTree(str);
+        if(i != str.length()){
+            throw new Exception("Неправильно составлен пример!");
+        }
+    }
+
+    public Node buildTree(String str){
+        char c = str.charAt(i);
+        i++;
+        Node root = new Node(' ', null, null);
+        if(c>= '0' && c <= '9'){
+            root.setInfo(c);
+        }
+        else{
+            root.setLeft(buildTree(str));
+            root.setInfo(str.charAt(i));
+            i++;
+            root.setRight(buildTree(str));
+            c = str.charAt(i);
+            i++;
+        }
+        return root;
+    }
+
+    public int calculate() throws Exception{
+        return calculate_rec(head);
+    }
+
+    private int calculate_rec(Node tree) throws Exception{
+        int result=0;
+        if(tree.getLeft() == null && tree.getRight()==null){
+            result = tree.getInfo() - '0';
+        }
+        else{
+            int left = calculate_rec(tree.getLeft());
+            int right = calculate_rec(tree.getRight());
+            switch (tree.getInfo()) {
+                case '+':
+                    result = left+right;
+                    break;
+                case '-':
+                    result = left-right;
+                    break;
+                case '*':
+                    result = left*right;
+                    break;
+                case '/':
+                    if(right == 0)
+                        throw new Exception("Деление на 0");
+                    else
+                        result = left/right;
+                default:
+                    break;
+            }
+        }
+        return result;
+    }
+}
